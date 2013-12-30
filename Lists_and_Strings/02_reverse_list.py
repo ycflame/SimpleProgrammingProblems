537c2fc906bf90e8d22a258fee2ba4406139d8ee
YCFlame <yangchao.cs@gmail.com>
2013-12-30 18:43:14 +0800

    define function

--- Lists_and_Strings/02_reverse_list.py
+++ Lists_and_Strings/02_reverse_list.py
@@ -1,5 +1,9 @@
 # coding:utf-8
 
+
+def reverse(example):
+    return example[::-1]
+
 if __name__ == '__main__':
     example = [3, 5, 2, 45, 204, 3, 54]
-    print example[::-1]
+    print reverse(example)
