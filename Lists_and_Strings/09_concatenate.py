8b09f3adb25615ffb17fa5abdca2758ed0820f0e
YCFlame <yangchao.cs@gmail.com>
2014-01-05 23:43:34 +0800

    List No.9

---
+++ Lists_and_Strings/09_concatenate.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+
+def concatenate(t1, t2):
+    return t1 + t2
+
+if __name__ == '__main__':
+    t1 = [1, 2, 3, 4]
+    t2 = [5, 6, 7, 8]
+    print concatenate(t1, t2)
